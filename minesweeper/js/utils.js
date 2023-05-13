c5351bba34cae7d2c12ca80922c83e87ddc7283c
Here East <nastiapiven@gmail.com>
2023-05-13 13:31:05 +0200

    feat: add get mines functionality

--- minesweeper/js/utils.js
+++ minesweeper/js/utils.js
@@ -1,10 +1,28 @@
-export { positionFree };
+export { positionExists, getMinesPositions };
 
-// Position free
-function positionFree(arr, el) {
+// GET ARRAY OFF MINES
+function getMinesPositions(size, minesCount) {
+  const positions = [];
+
+  while (positions.length < minesCount) {
+    const pos = {
+      x: Math.floor(Math.random() * size),
+      y: Math.floor(Math.random() * size)
+    };
+
+    if (!positionExists(positions, pos)) {
+      positions.push(pos);
+    }
+  }
+
+  return positions;
+}
+
+// POSITION EXISTS IN ARRAY
+function positionExists(arr, el) {
   const posExists = arr.some((arrElement) => {
     return arrElement.x === el.x && arrElement.y === el.y;
   });
 
-  return !posExists;
+  return posExists;
 }
