86fefcf183c7ed31d15c513489537ce64a0821a0
Here East <nastiapiven@gmail.com>
2023-05-13 15:53:49 +0200

    fix: add end game basic functionality

---
+++ minesweeper/js/popup.js
@@ -0,0 +1,40 @@
+// END GAME
+function endGame(success, moves) {
+  let message;
+
+  if (success) message = `Hooray! You found all mines in ${moves} moves!`;
+  if (!success) message = `Game over. Try again!`;
+
+  showPopup(message);
+}
+
+//
+// SHOW POPUP
+function showPopup(message) {
+  const popup = document.querySelector(".popup");
+  const popupMessage = document.querySelector(".popup__message");
+
+  popupMessage.textContent = message;
+
+  popup.style.display = "flex";
+  popup.style.opacity = 1;
+
+  document.body.style.overflow = "hidden";
+}
+
+//
+// CLOSE POPUP
+function closePopup() {
+  const popup = document.querySelector(".popup");
+  const popupMessage = document.querySelector(".popup__message");
+
+  popupMessage.textContent = "";
+
+  popup.style.display = "none";
+  popup.style.opacity = 0;
+
+  document.body.style.overflow = "";
+}
+
+//
+export { endGame, closePopup };
--- minesweeper/script.js
+++ minesweeper/script.js
@@ -2,10 +2,14 @@ import { toggleMode } from "./js/mode.js";
 import { createPage } from "./js/page.js";
 import { createBoard } from "./js/board.js";
 import { getMinesPositions, positionExists } from "./js/utils.js";
+import { endGame, closePopup } from "./js/popup.js";
 
-let size = 8;
-let minesCount = 4;
+let size = 4;
+let minesCount = 1;
 let moves = 0;
+
+let success;
+
 
 //
 createPage();
@@ -20,6 +24,8 @@ const board = document.querySelector(".board");
 const buttonMode = document.querySelector(".button__toggle-mode");
 const buttons = Array.from(document.querySelectorAll(".button__tile"));
 const movesElement = document.querySelector(".moves");
+const buttonPopupClose = document.querySelector(".button__popup--close");
+
 
 //
 // EVENTS
@@ -29,6 +35,8 @@ buttons.forEach((button) => {
   button.addEventListener("click", handleLeftClick);
   button.addEventListener("contextmenu", handleRightClick);
 });
+
+buttonPopupClose.addEventListener("click", closePopup);
 
 //
 //
@@ -57,6 +65,49 @@ function handleRightClick(e) {
 }
 
 //
+// OPEN TILE
+function openTile(tile) {
+  const clickedTile = {
+    x: Number(tile.dataset.x),
+    y: Number(tile.dataset.y)
+  };
+
+  const isMine = positionExists(minePositions, clickedTile);
+  const nearTiles = getNearTiles(clickedTile);
+
+  if (isMine) {
+    tile.dataset.state = "mine";
+
+    success = false;
+    endGame(success);
+
+  } else {
+    let nearMines = 0;
+
+    nearTiles.forEach((nearTile) => {
+      const isMine = positionExists(minePositions, nearTile);
+      if(isMine) nearMines += 1;
+    })
+
+    if (nearMines) {
+      tile.textContent = nearMines;
+      tile.dataset.state = "number";
+    } else {
+      tile.dataset.state = "number";
+    }
+
+    const closedTiles = buttons.filter((button) => {
+      return button.dataset.state === "hidden" || button.dataset.state === "marked";
+    })
+
+    if(closedTiles.length === minesCount) {
+      success = true;
+      endGame(success, moves);
+    }
+  }
+}
+
+//
 // NEAR TILES
 function getNearTiles(tile) {
   let nearTiles = [];
@@ -74,40 +125,6 @@ function getNearTiles(tile) {
 
   return nearTiles;
 }
-
-//
-// OPEN TILE
-function openTile(tile) {
-
-  const clickedTile = {
-    x: Number(tile.dataset.x),
-    y: Number(tile.dataset.y)
-  };
-
-  const isMine = positionExists(minePositions, clickedTile);
-  console.log("Is mine: ", isMine);
-
-  const nearTiles = getNearTiles(clickedTile);
-
-  if(isMine) {
-    tile.dataset.state = "mine";
-  } else {
-    let minesCount = 0;
-
-    nearTiles.forEach((nearTile) => {
-      const isMine = positionExists(minePositions, nearTile);
-      if(isMine) minesCount += 1;
-    })
-
-    if (minesCount) {
-      tile.textContent = minesCount;
-      tile.dataset.state = "number";
-    } else {
-      tile.dataset.state = "number";
-    }
-  }
-}
-
 
 //
 // MARK TILES
