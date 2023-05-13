33107a6caea8923ab5cd9f32307a36a2b72900d5
Here East <nastiapiven@gmail.com>
2023-05-13 13:29:40 +0200

    feat: add new header with header information

--- minesweeper/js/page.js
+++ minesweeper/js/page.js
@@ -4,27 +4,43 @@ function createPage() {
   const page = document.createElement("div");
   page.classList.add("page");
 
-  const game = document.createElement("div");
-  game.classList.add("game");
+  const header = createHeader();
 
-  const header = document.createElement("div");
-  header.classList.add("game__header");
+  const main = document.createElement("main");
+  main.classList.add("game__board", "main");
 
-  const title = document.createElement("h1");
-  title.textContent = "Minesweeper";
+  const title = `<h1>Minesweeper</h1>`;
+  main.insertAdjacentHTML("afterbegin", title);
 
-  const board = document.createElement("div");
-  board.classList.add("game__board");
-
-  const modeButton = document.createElement("button");
-  modeButton.classList.add("button__mode");
-  modeButton.textContent = "Toggle Mode";
-
-  header.append(title, modeButton);
-  game.append(header, board);
-  page.append(game);
+  page.insertAdjacentHTML("afterbegin", header);
+  page.append(main);
 
   document.body.prepend(page);
+}
+
+function createHeader() {
+  const header = `
+    <header class="header">
+      <div class="header__items">
+        <div class="header__item">
+          <span>Moves:</span><span class="moves">0</span>
+        </div>
+        <div class="header__item">
+          <span>🚩Marked:</span><span class="moves">0</span>
+        </div>
+        <div class="header__item">
+          <span>🚩Remained:</span><span class="seconds">0</span>
+        </div>
+        <div class="header__item">
+          <span>Seconds:</span><span class="seconds">0</span>
+        </div>
+      </div>
+      <div class="header__buttons">
+        <button class="button__toggle-mode">Toggle Mode</button>
+      </div>
+    </header>`;
+
+  return header;
 }
 
 export { createPage };
