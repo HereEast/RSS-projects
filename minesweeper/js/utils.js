f218a170de99f0e329117948eba271c304893a9e
Here East <nastiapiven@gmail.com>
2023-05-17 13:46:38 +0200

    refactor: move util functions to util.js

--- minesweeper/js/utils.js
+++ minesweeper/js/utils.js
@@ -1,44 +1,11 @@
-export { positionExists, getMinesPositions, getTilePosition };
+export { isSuccess };
 
 //
-// GET MINES
-function getMinesPositions(size, minesCount) {
-  const firstTile = document.querySelector("[data-state='number']");
-  const positions = [];
-
-  while (positions.length < minesCount) {
-    const pos = {
-      x: Math.floor(Math.random() * size),
-      y: Math.floor(Math.random() * size)
-    };
-
-    const isFirstTile = pos.x === firstTile.x && pos.y === firstTile.y;
-
-    if (!positionExists(positions, pos) && !isFirstTile) {
-      positions.push(pos);
-    }
-  }
-
-  return positions;
-}
-
-//
-// GET TILE POS
-function getTilePosition(tile) {
-  const pos = {
-    x: Number(tile.dataset.x),
-    y: Number(tile.dataset.y)
-  };
-
-  return pos;
-}
-
-//
-// POSITION EXISTS IN ARRAY
-function positionExists(arr, el) {
-  const posExists = arr.some((arrElement) => {
-    return arrElement.x === el.x && arrElement.y === el.y;
+// CHECK SUCCESS
+function isSuccess(tiles, minesCount) {
+  const closedTiles = tiles.filter((tile) => {
+    return tile.dataset.state === "hidden" || tile.dataset.state === "marked";
   });
 
-  return posExists;
+  return closedTiles.length === minesCount;
 }
