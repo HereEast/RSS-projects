648a3cfbe90e7b8d94ec6590072645348aaa2abb
Here East <nastiapiven@gmail.com>
2023-05-18 22:48:05 +0200

    refactor: refactor createBoard func

--- minesweeper/js/board.js
+++ minesweeper/js/board.js
@@ -11,13 +11,39 @@ function createBoard(size) {
   board.classList.add("board");
   board.style.setProperty("--size", size);
 
-  const grid = createGrid(size);
 
-  grid.forEach((row) => {
-    row.forEach((element) => {
-      board.append(element.tile);
-    });
-  });
+  for (let x = 0; x < size; x++) {
+    // const row = [];
+
+    for (let y = 0; y < size; y++) {
+      const tile = document.createElement("button");
+      tile.classList.add("button__tile");
+
+      tile.dataset.state = "hidden";
+      tile.dataset.x = y;
+      tile.dataset.y = x;
+
+      // const tileData = {
+      //   tile: tile,
+      //   x: x,
+      //   y: y
+      // };
+
+      // row.push(tileData);
+
+      board.append(tile);
+    }
+
+    // grid.push(row);
+  }
+
+  // const grid = createGrid(size);
+
+  // grid.forEach((row) => {
+  //   row.forEach((element) => {
+  //     board.append(element.tile);
+  //   });
+  // });
 
   boardContainer.append(board);
 }
