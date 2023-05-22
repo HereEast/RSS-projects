743b4eada7ffb70b701858b44588f6be871d3ca4
Here East <nastiapiven@gmail.com>
2023-05-22 18:47:47 +0200

    feat: add eslint file

---
+++ minesweeper/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["airbnb-base", "airbnb-base/legacy"],
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        "import/extensions": [
+          "off",
+          "always",
+          {
+            ignorePackages: true
+          }
+        ]
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  rules: {
+    quotes: ["error", "double"],
+    "import/prefer-default-export": ["off" || "warn" || "error"],
+    "no-use-before-define": [
+      "error",
+      {
+        functions: false
+      }
+    ],
+    "no-console": ["allow"]
+  }
+};
