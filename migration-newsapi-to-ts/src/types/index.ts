87afbb0b87929d31de22a67b780f3a1e89e2a8d5
Here East <nastiapiven@gmail.com>
2023-06-10 00:48:44 +0200

    feat: add Article, Data type and RenderNews interface

--- migration-newsapi-to-ts/src/types/index.ts
+++ migration-newsapi-to-ts/src/types/index.ts
@@ -1,1 +1,19 @@
+export type Article = {
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string;
+  source: {
+    id: string;
+    name: string;
+  };
+  title: string;
+  url: string;
+  urlToImage: string;
+};
 
+export type Data = Array<Article>;
+
+export interface IRenderNews {
+  draw(data: Data): void;
+}
