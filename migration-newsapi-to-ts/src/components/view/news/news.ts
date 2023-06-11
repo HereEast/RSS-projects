9ba6c3cce1c071a83b2134e35363987143793eff
Here East <nastiapiven@gmail.com>
2023-06-11 18:48:13 +0200

    feat: add types and enums to news

--- migration-newsapi-to-ts/src/components/view/news/news.ts
+++ migration-newsapi-to-ts/src/components/view/news/news.ts
@@ -1,55 +1,38 @@
 import "./news.css";
-import { Data, IRenderNews } from "../../../types";
+import { Article, NewsData, IRenderData } from "../../../types";
+import { Selector } from "../../../types/enums";
 
-class News implements IRenderNews {
-  draw(data: Data): void {
-    const news =
-      data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+class News implements IRenderData {
+  draw(data: NewsData["articles"] | []): void {
+    let news: NewsData["articles"];
 
-    const fragment = document.createDocumentFragment();
+    if (data.length >= 10) {
+      news = data.filter((el: Article, idx: number) => idx < 10);
+    } else {
+      news = data;
+    }
 
-    const newsItemTemp = document.querySelector(
-      "#newsItemTemp"
-    ) as HTMLTemplateElement;
+    const fragment = document.createDocumentFragment() as DocumentFragment;
 
-    const newsContainer = document.querySelector(".news") as HTMLElement;
+    const newsItemTemp = document.querySelector(Selector.NewsTemplate) as HTMLTemplateElement;
+    const newsContainer = document.querySelector(Selector.NewsContainer) as HTMLElement;
 
-    news.forEach((item, idx) => {
+    news.forEach((item: Article, idx: number) => {
       const newsClone = newsItemTemp.content.cloneNode(true) as HTMLElement;
-      const newsItem = newsClone.querySelector(".news__item") as HTMLElement;
-      const photo = newsClone.querySelector(".news__meta-photo") as HTMLElement;
-      const author = newsClone.querySelector(
-        ".news__meta-author"
-      ) as HTMLElement;
-
-      const date = newsClone.querySelector(".news__meta-date") as HTMLElement;
-      const title = newsClone.querySelector(
-        ".news__description-title"
-      ) as HTMLElement;
-
-      const source = newsClone.querySelector(
-        ".news__description-source"
-      ) as HTMLElement;
-
-      const description = newsClone.querySelector(
-        ".news__description-content"
-      ) as HTMLElement;
-
-      const link = newsClone.querySelector(
-        ".news__read-more a"
-      ) as HTMLLinkElement;
+      const newsItem = newsClone.querySelector(Selector.NewsItem) as HTMLElement;
+      const photo = newsClone.querySelector(Selector.Photo) as HTMLElement;
+      const author = newsClone.querySelector(Selector.Author) as HTMLElement;
+      const date = newsClone.querySelector(Selector.Date) as HTMLElement;
+      const title = newsClone.querySelector(Selector.Title) as HTMLElement;
+      const source = newsClone.querySelector(Selector.Source) as HTMLElement;
+      const description = newsClone.querySelector(Selector.Description) as HTMLElement;
+      const link = newsClone.querySelector(Selector.Link) as HTMLLinkElement;
 
       if (idx % 2) newsItem.classList.add("alt");
 
-      photo.style.backgroundImage = `url(${
-        item.urlToImage || "img/news_placeholder.jpg"
-      })`;
+      photo.style.backgroundImage = `url(${item.urlToImage || "img/news_placeholder.jpg"})`;
       author.textContent = item.author || item.source.name;
-      date.textContent = item.publishedAt
-        .slice(0, 10)
-        .split("-")
-        .reverse()
-        .join("-");
+      date.textContent = item.publishedAt.slice(0, 10).split("-").reverse().join("-");
 
       title.textContent = item.title;
       source.textContent = item.source.name;
