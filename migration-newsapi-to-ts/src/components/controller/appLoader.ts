081e28b0fa2227104b5a0ac2570821dd09e01015
Here East <nastiapiven@gmail.com>
2023-06-13 00:42:39 +0200

    feat: add responsive layout

--- migration-newsapi-to-ts/src/components/controller/appLoader.ts
+++ migration-newsapi-to-ts/src/components/controller/appLoader.ts
@@ -4,7 +4,7 @@ import { ILoader } from "../../types/interfaces";
 class AppLoader extends Loader implements Partial<ILoader> {
   constructor() {
     super("https://newsapi.org/v2/", {
-      apiKey: "a7e202826fa24c73b3c0aeda8a237ea6",
+      apiKey: "8eb6726de49848bc9113f5d132c3d548",
     });
   }
 }
