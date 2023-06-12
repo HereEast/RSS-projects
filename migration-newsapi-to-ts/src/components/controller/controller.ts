a984b7f028d6b560e33f48313c7208e0d60e7705
Here East <nastiapiven@gmail.com>
2023-06-12 17:30:48 +0200

    feat: migrate controller.js

--- migration-newsapi-to-ts/src/components/controller/controller.ts
+++ migration-newsapi-to-ts/src/components/controller/controller.ts
@@ -1,15 +1,18 @@
 import AppLoader from "./appLoader";
 import { IController } from "../../types/interfaces";
-import { NewsData, SourcesData } from "../../types/types";
+import { Data } from "../../types/types";
 
-class AppController extends AppLoader implements IController {
-  getSources(callback: (data: SourcesData) => void): void {
+class AppController extends AppLoader implements Readonly<IController> {
+  getSources(callback: (data: Data) => void): void {
     super.getResp({ endpoint: "sources" }, callback);
   }
 
-  getNews(e: Event, callback: (data: NewsData) => void): void {
-    let target = e.target as HTMLElement;
-    const newsContainer = e.currentTarget as HTMLElement;
+  getNews(e: Event, callback: (data: Data) => void): void {
+    let target = e.target as HTMLElement | null;
+    const newsContainer = e.currentTarget as HTMLElement | null;
+
+    if (!target) throw Error("Can't find e.target element.");
+    if (!newsContainer) throw Error("Can't find e.currentTarget element.");
 
     while (target !== newsContainer) {
       if (target.classList.contains("source__item")) {
@@ -29,7 +32,11 @@ class AppController extends AppLoader implements IController {
         }
         return;
       }
-      target = target.parentNode as HTMLElement;
+
+      const parentNode = target.parentNode as HTMLElement | null;
+      if (!parentNode) throw Error("Can't find target.parentNode.");
+
+      target = parentNode;
     }
   }
 }
