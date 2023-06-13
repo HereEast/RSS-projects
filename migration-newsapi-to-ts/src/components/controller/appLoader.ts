f9ffc93933f1e1dd9e54bf0f6e24e3d4259d4bb4
Here East <nastiapiven@gmail.com>
2023-06-13 10:27:22 +0200

    feat: update link

--- migration-newsapi-to-ts/src/components/controller/appLoader.ts
+++ migration-newsapi-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import { ILoader } from "../../types/interfaces";
 
 class AppLoader extends Loader implements Partial<ILoader> {
   constructor() {
-    super("https://newsapi.org/v2/", {
+    super("https://nodenews.herokuapp.com/", {
       apiKey: "8eb6726de49848bc9113f5d132c3d548",
     });
   }
