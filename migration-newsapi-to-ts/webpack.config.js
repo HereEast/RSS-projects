b6db7de274bd8ed6b9028d9b32fc92feb30018dc
Here East <nastiapiven@gmail.com>
2023-06-13 12:21:49 +0200

    feat: edit webpack.config

--- migration-newsapi-to-ts/webpack.config.js
+++ migration-newsapi-to-ts/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 
-// const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const EslintPlugin = require("eslint-webpack-plugin");
@@ -53,10 +52,3 @@ module.exports = {
     new EslintPlugin({ extensions: "ts" }),
   ],
 };
-
-// module.exports = ({ mode }) => {
-//   const isProductionMode = mode === "prod";
-//   const envConfig = isProductionMode ? require("./webpack.prod.config") : require("./webpack.dev.config");
-
-//   return merge(baseConfig, envConfig);
-// };
