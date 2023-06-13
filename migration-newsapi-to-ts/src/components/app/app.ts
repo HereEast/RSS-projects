190a5033675fd2413aa36432687e09d4012684b4
Here East <nastiapiven@gmail.com>
2023-06-13 21:13:31 +0200

    fix: edit app

--- migration-newsapi-to-ts/src/components/app/app.ts
+++ migration-newsapi-to-ts/src/components/app/app.ts
@@ -1,10 +1,9 @@
 import AppController from "../controller/controller";
 import { Selector } from "../../types/enums";
 import { AppView } from "../view/appView";
-import { IApp } from "../../types/interfaces";
 import { Data } from "../../types/types";
 
-class App implements Readonly<IApp> {
+class App {
   private controller: AppController;
   private view: AppView;
 
@@ -14,26 +13,29 @@ class App implements Readonly<IApp> {
   }
 
   start(): void {
-    const sources = document.querySelector(Selector.SourcesContainer) as HTMLElement | null;
+    const sources = document.querySelector(Selector.SourcesContainer);
     if (!sources) throw Error(`Can't find DOM element ${Selector.SourcesContainer}`);
 
-    const sourcesTabs = sources.children as HTMLCollection;
+    const sourcesTabs = sources.children;
     if (!sourcesTabs) throw Error(`Can't find children of ${Selector.SourcesContainer}`);
 
     sources.addEventListener("click", (e: Event) => {
-      const target = e.target as HTMLElement | null;
-      const targetContainer = target?.closest(Selector.SourceItem) as HTMLElement | null;
+      const target = e.target;
 
       if (!target) throw Error("Can't find e.target element.");
-      if (!targetContainer) throw Error(`Can't find tDOM element ${Selector.SourceItem}.`);
+      if (!(target instanceof HTMLElement)) return;
 
-      [...sourcesTabs].forEach((tab: Element) => tab.classList.remove("active"));
+      const targetContainer = target.closest(Selector.SourceItem);
+
+      if (!targetContainer) throw Error(`Can't find DOM element ${Selector.SourceItem}.`);
+
+      [...sourcesTabs].forEach((tab) => tab.classList.remove("active"));
       targetContainer.classList.add("active");
 
-      this.controller.getNews(e, (data: Data) => this.view.drawNews(data));
+      this.controller.getNews(e, (data: Data) => this.view.renderNews(data));
     });
 
-    this.controller.getSources((data: Data) => this.view.drawSources(data));
+    this.controller.getSources((data: Data) => this.view.renderSources(data));
   }
 }
 
