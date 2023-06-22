2745885861917d6e73ed9eadc91d3c7e04ba88fc
Here East <nastiapiven@gmail.com>
2023-06-22 18:54:07 +0200

    feat: add game view

---
+++ rs-css-selectors/src/components/ui/game-view.ts
@@ -0,0 +1,19 @@
+import { createElement } from "../../utils/element";
+import { createHeader } from "./header";
+import { createTaskBlock } from "./task-view";
+import { createCodeBlock } from "./editors-view";
+
+// Game section
+export function createGameSection(): HTMLElement {
+  const gameSection = createElement("section", ["main__section", "section__game"]);
+  const header = createHeader();
+
+  const game = createElement("div", ["game"]);
+  const taskBlock = createTaskBlock();
+  const codeBlock = createCodeBlock();
+
+  game.append(taskBlock, codeBlock);
+  gameSection.append(header, game);
+
+  return gameSection;
+}
