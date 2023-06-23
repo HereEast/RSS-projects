8ebfb415cbb7e194d24c87aae4b66ce60643bb25
Here East <nastiapiven@gmail.com>
2023-06-23 13:36:37 +0200

    refactor: refactor footer

--- rs-css-selectors/src/components/footer.ts
+++ rs-css-selectors/src/components/footer.ts
@@ -2,18 +2,18 @@ import { createElement } from "../utils/element";
 
 export function createFooter(): HTMLElement {
   const footer = createElement("footer", ["footer", "content-width"]);
-  const footerContainer = createElement("div", ["footer__container"]);
 
   const linkRS = "https://rs.school/";
   const linkGH = "https://github.com/HereEast";
 
-  const copyright = `<span>2023, <a href="${linkRS}" target="_blank">RS School</a> — RS CSS Selectors</span>`;
-  const github = `<a href="${linkGH}" target="_blank">Here East</a>`;
+  const footerContent = `
+    <div class="footer__container">
+      <span>2023, <a href="${linkRS}" target="_blank">RS School</a> — RS CSS Selectors</span>
+      <a href="${linkGH}" target="_blank">Here East</a>
+    </div>
+  `;
 
-  footerContainer.insertAdjacentHTML("afterbegin", github);
-  footerContainer.insertAdjacentHTML("afterbegin", copyright);
-
-  footer.append(footerContainer);
+  footer.insertAdjacentHTML("afterbegin", footerContent);
 
   return footer;
 }
