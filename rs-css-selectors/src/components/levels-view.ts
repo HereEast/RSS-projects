65af3c74bca712716fff6fd6da8fe066237ef98a
Here East <nastiapiven@gmail.com>
2023-06-23 13:10:30 +0200

    refactor: rename files

--- rs-css-selectors/src/components/levels-view.ts
+++
@@ -1,8 +0,0 @@
-import { createElement } from "../utils/element";
-
-// Levels
-export function createLevelsSection(): HTMLElement {
-  const levelsPanel = createElement("section", ["main__section", "section__levels"]);
-
-  return levelsPanel;
-}
