be07f2995de4c4c2cc0163ad6e175d960d0eb040
Here East <nastiapiven@gmail.com>
2023-06-23 13:36:51 +0200

    refactor: refactor header

--- rs-css-selectors/src/components/header.ts
+++ rs-css-selectors/src/components/header.ts
@@ -5,38 +5,45 @@ export function createHeader(): HTMLElement {
   const title = createTitle();
   const buttons = createHeaderButtons();
 
-  console.log(header.outerHTML);
-
-  header.append(title, buttons);
+  header.insertAdjacentHTML("afterbegin", title);
+  header.insertAdjacentHTML("beforeend", buttons);
 
   return header;
 }
 
 // Title
-function createTitle(): HTMLElement {
-  const titleContainer = createElement("div", ["header__title"]);
-  const title = createElement("h1", ["title__text"], "CSS Playground");
-  const icon = createElement("div", ["title__icon"]);
-
-  icon.innerHTML = `
+function createTitle(): string {
+  const SVGIcon = `
     <svg class="icon" xmlns="http://www.w3.org/2000/svg">
       <path d="M7.05037 0L2.5332e-07 12H7.05037L14 11.8286L7.05037 0Z" fill="#6BCB5B"/>
       <circle cx="36.5" cy="6.5" r="6.5" fill="#FFE208"/>
       <rect x="16" width="12" height="12" fill="#725CFA"/>
-    </svg>`;
+    </svg>
+  `;
 
-  titleContainer.append(title, icon);
+  const headerTitle = `
+    <div class="header__title">
+      <h1 class='title__text'>CSS Playground</h1>
+      <div class="title__icon">
+        ${SVGIcon}
+      </div>
+    </div>  
+  `;
 
-  return titleContainer;
+  return headerTitle;
 }
 
 // Buttons
-function createHeaderButtons(): HTMLElement {
-  const headerButtons = createElement("div", ["header__buttons"]);
-  const buttonHint = createElement("button", ["header__button", "button--hint"], "Hint");
-  const buttonReset = createElement("button", ["header__button", "button--reset"], "Reset");
+function createHeaderButtons(): string {
+  const buttonHint = "<button class='button button--hint'>Hint</button>";
+  const buttonReset = "<button class='button button--reset'>Reset</button>";
 
-  headerButtons.append(buttonHint, buttonReset);
+  const headerButtons = `
+    <div class="header__buttons">
+      ${buttonHint}
+      ${buttonReset}
+    </div>
+  `;
 
   return headerButtons;
 }
