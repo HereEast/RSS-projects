f373add390115601ad66ceb8a1a5cabe8d4bc5bb
Here East <nastiapiven@gmail.com>
2023-06-26 22:49:10 +0200

    feat: add new data to Selector enum

--- rs-css-selectors/src/types/enums.ts
+++ rs-css-selectors/src/types/enums.ts
@@ -2,4 +2,8 @@ export const enum Selector {
   ShowLevelsBtn = ".button--levels",
   HideLevelsBtn = ".button--hide",
   LevelsPanel = ".section__levels",
+  LevelsContainer = ".levels__container",
+  LevelItem = ".level__item",
+  PanelHeaderLevelNumber = ".section__levels .header__data--level",
+  MobileHeaderLevelNumber = ".header .header__data--level",
 }
