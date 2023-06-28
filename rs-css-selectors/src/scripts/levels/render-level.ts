fae3328925271ac1509dc10510bd5f91bb183ca0
Here East <nastiapiven@gmail.com>
2023-06-28 18:47:48 +0200

    feat: add level class to body element

--- rs-css-selectors/src/scripts/levels/render-level.ts
+++ rs-css-selectors/src/scripts/levels/render-level.ts
@@ -1,12 +1,18 @@
 import { Levels } from "../../types/types";
 import { highlightLevel } from "./highlight-item";
 import { updateLevelCount } from "./update-count";
-import { updateTaskTitle } from "./task-title";
+import { updateTaskName } from "./level-task";
+import { updateHTMLEditor } from "./level-html-content";
+import { updateGraphics } from "./level-graphics";
 
 // Render selected level
 export function renderSelectedLevel(id: string, data: Levels): void {
+  document.body.className = `level--${id}`;
+
   highlightLevel(id);
 
   updateLevelCount(id);
-  updateTaskTitle(id, data);
+  updateTaskName(id, data);
+  updateHTMLEditor(id, data);
+  updateGraphics(id, data);
 }
