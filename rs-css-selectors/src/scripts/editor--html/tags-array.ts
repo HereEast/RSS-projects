1d2a109789640efbea44a6c72d9538d7bbd3ab4c
Here East <nastiapiven@gmail.com>
2023-06-28 18:44:25 +0200

    refactor: delete files

--- rs-css-selectors/src/scripts/editor--html/tags-array.ts
+++
@@ -1,9 +0,0 @@
-// Array of tags line by line
-export function getTagsArray(code: string): string[] {
-  const arr = code
-    .split("\n")
-    .map((str) => str.trim())
-    .filter((str) => str.length);
-
-  return arr;
-}
