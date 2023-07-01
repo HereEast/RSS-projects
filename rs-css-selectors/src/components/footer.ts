fddfec79b6e2a9223808df803c29d6338b1287a6
Here East <nastiapiven@gmail.com>
2023-07-01 20:54:55 +0200

    refactor: refactor Components/footer

--- rs-css-selectors/src/components/footer.ts
+++ rs-css-selectors/src/components/footer.ts
@@ -6,14 +6,14 @@ export function createFooter(): HTMLElement {
   const linkRS = "https://rs.school/";
   const linkGH = "https://github.com/HereEast";
 
-  const footerContent = `
+  const footerContainer = `
     <div class="footer__container">
       <span>2023, <a href="${linkRS}" target="_blank">RS School</a> — RS CSS Selectors</span>
       <a href="${linkGH}" target="_blank">Here East</a>
     </div>
   `;
 
-  footer.insertAdjacentHTML("afterbegin", footerContent);
+  footer.insertAdjacentHTML("afterbegin", footerContainer);
 
   return footer;
 }
