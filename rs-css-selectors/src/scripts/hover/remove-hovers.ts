5116cff41808975fbc154e943112266256694e0e
Here East <nastiapiven@gmail.com>
2023-07-03 18:24:54 +0200

    feat: add remove all hover states func

---
+++ rs-css-selectors/src/scripts/hover/remove-hovers.ts
@@ -0,0 +1,11 @@
+import { Selector } from "../../types/enums";
+import { getElementsArray } from "../../utils/get-element";
+
+// Remove hover
+export function removeHoverStates(): void {
+  const tooltips = getElementsArray(Selector.Tooltip);
+  const lines = getElementsArray(Selector.HTMLLine);
+
+  lines.forEach((line) => line.classList.remove("highlight"));
+  tooltips.forEach((tooltip) => tooltip.remove());
+}
