a61abf636e4a4fa69b87608ec6336f5b73c421ff
Here East <nastiapiven@gmail.com>
2023-07-19 01:04:22 +0200

    feat: create header buttons

---
+++ async-race/src/components/header/header-buttons.ts
@@ -0,0 +1,18 @@
+import { ButtonName, Selector } from "../../_types/enums";
+import { createElement } from "../../scripts/_utils/create-element";
+
+// Header buttons
+export function createHeaderButtons(): HTMLElement {
+  const container = createElement("div", [Selector.HeaderButtons]);
+  const bubbleElement = createElement("span", [Selector.Bubble]);
+
+  const garageButton = createElement("button", [Selector.ViewButton], ButtonName.Garage);
+  const winnersButton = createElement("button", [Selector.ViewButton], ButtonName.Winners);
+
+  garageButton.id = Selector.ButtonGarage.slice(1);
+  winnersButton.id = Selector.ButtonWinners.slice(1);
+
+  container.append(bubbleElement, garageButton, winnersButton);
+
+  return container;
+}
