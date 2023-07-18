dea238991bc130efecc9fce38979ff25db792df0
Here East <nastiapiven@gmail.com>
2023-07-19 01:04:57 +0200

    feat: add toggle header buttons func

---
+++ async-race/src/scripts/listeners.ts
@@ -0,0 +1,14 @@
+import { Selector } from "../_types/enums";
+import { getElement } from "./_utils/get-element";
+import { renderGarageView } from "./view-garage/render-garage";
+import { renderWinnersView } from "./view-winners/render-winners";
+
+// Listeners
+export function initListeners(): void {
+  const garageButton = getElement(Selector.ButtonGarage);
+  const winnersButton = getElement(Selector.ButtonWinners);
+
+  // Events
+  garageButton.addEventListener("click", renderGarageView);
+  winnersButton.addEventListener("click", renderWinnersView);
+}
---
+++ async-race/src/scripts/menu/set-active-button.ts
@@ -0,0 +1,25 @@
+import { getElement, getElementsArray } from "../_utils/get-element";
+import { ButtonName, Selector } from "../../_types/enums";
+
+// Active button
+export function setActiveButton(button?: HTMLElement): void {
+  const bubble = getElement(Selector.Bubble);
+
+  const buttons = getElementsArray(Selector.ViewButton);
+  buttons.forEach((btn) => btn.classList.remove("active"));
+
+  if (!button) {
+    const activeButton = getElement(Selector.ButtonGarage);
+    activeButton.classList.add("active");
+    bubble.classList.remove("winners");
+    return;
+  }
+
+  button?.classList.add("active");
+
+  if (button?.textContent?.includes(ButtonName.Garage)) {
+    bubble.classList.remove("winners");
+  } else {
+    bubble.classList.add("winners");
+  }
+}
