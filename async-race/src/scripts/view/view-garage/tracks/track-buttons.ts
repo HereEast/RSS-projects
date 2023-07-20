dcf3e48a9ecadb7ee5c372287b5ccbfeb2c5d89f
Here East <nastiapiven@gmail.com>
2023-07-21 01:59:35 +0200

    feat: update button creation and adding event handlers

--- async-race/src/scripts/view/view-garage/tracks/track-buttons.ts
+++ async-race/src/scripts/view/view-garage/tracks/track-buttons.ts
@@ -1,17 +1,39 @@
-import { ButtonName, Selector } from "../../../../types/enums";
+import { ButtonName, Selector, Callback } from "../../../../types/types";
 import { createElement } from "../../../utils/create-element";
+import { handleDelete } from "../handlers/handle-delete";
 
-// Track buttons
+// Button
+export function createButton(name: ButtonName, callback: Callback): HTMLElement {
+  const button = createElement("button", [Selector.ButtonTrack], name);
+  const buttonID = `button__${name.toLowerCase()}`;
+
+  button.classList.add(buttonID);
+  button.id = buttonID;
+
+  button.addEventListener("click", callback);
+
+  return button;
+}
+
+// Buttons
 export function createTrackButtons(): HTMLElement {
   const trackButtons = createElement("div", [Selector.TrackButtons]);
 
-  const buttonsNames = [ButtonName.Start, ButtonName.Stop, ButtonName.Edit, ButtonName.Delete];
+  const startBtn = createButton(ButtonName.Start, (e) => console.log(e.target));
+  const stopBtn = createButton(ButtonName.Stop, (e) => console.log(e.target));
+  const editBtn = createButton(ButtonName.Edit, (e) => console.log(e.target));
+  const deleteBtn = createButton(ButtonName.Delete, handleDelete);
 
-  buttonsNames.forEach((name) => {
-    const buttonClass = `.button__${name.toLowerCase()}`;
-    const button = createElement("button", [Selector.ButtonTrack, buttonClass], name);
-    trackButtons.append(button);
-  });
+  // buttonsNames.forEach((name) => {
+  //   const buttonID = `button__${name.toLowerCase()}`;
+  //   const button = createElement("button", [Selector.ButtonTrack], name);
+
+  //   button.classList.add(buttonID);
+  //   button.id = buttonID;
+
+  //   trackButtons.append(button);
+  // });
+  trackButtons.append(startBtn, stopBtn, editBtn, deleteBtn);
 
   return trackButtons;
 }
